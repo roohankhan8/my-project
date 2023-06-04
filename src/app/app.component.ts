6c4fcd42bf3e8d496b0c2639df14462a3bcf2bdb
Roohan Khan <roohank8@gmail.com>
2023-06-04 13:21:00 -0700

    First

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Car } from './cars';
+import { TransportationService } from './transportation.service';
 
 @Component({
   selector: 'my-app',
@@ -9,13 +10,24 @@ import { Car } from './cars';
 export class AppComponent {
   prefix = "I'm Roohan";
   name = 'Roohan';
-  // username: string;
+  username: string = '';
   colors: string[] = ['red', 'blue', 'green', 'purple'];
-  subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
-  honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
-  bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
-  cars: Car[] = [this.subaru, this.honda, this.bmw];
-  title: any;
+
+  // subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
+  // honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
+  // bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
+  // cars: Car[] = [this.subaru, this.honda, this.bmw];
+
+  make: string;
+  model: string;
+  miles: number;
+
+  cars: Car[] = [];
+
+  // title: any;
+  // transportationService: any;
+  // fruits: any;
+
   speak() {
     var sentence = this.prefix + this.name;
     return sentence;
@@ -31,6 +43,16 @@ export class AppComponent {
   update() {
     this.phrase += ' ..and going';
   }
-  username: string = '';
-  constructor(){}
+  constructor(private transportationService: TransportationService) {}
+
+  getCarsOut(): void {
+    this.cars = this.transportationService.getCars();
+  }
+  ngOnInit(): void {
+    this.getCarsOut();
+  }
+  addCar() {
+    const newCar: Car = { make: this.make, model: this.model, miles: this.miles};
+    this.transportationService.addCar(newCar);
+  }
 }
--- src/app/transportation.service.ts
+++ src/app/transportation.service.ts
@@ -1,21 +1,25 @@
 import { Injectable } from '@angular/core';
- import { Car } from './cars';
- 
- @Injectable({
-  providedIn: 'root'
- })
- export class TransportationService {
+import { Car } from './cars';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransportationService {
   // NEW CODE
-  subaru: Car = {make: 'Subaru', model: 'Outback', miles: 58232};
-  honda: Car = {make: 'Honda', model: 'Accord', miles: 39393};
-  bmw: Car = {make: 'BMW', model: 'X3', miles: 4400};
- 
-  cars:Car[] = [this.subaru, this.honda, this.bmw];
- 
-  constructor() { }
- 
+  subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
+  honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
+  bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
+
+  addCar(car: Car) {
+    this.cars.push(car);
+  }
+  
+  cars: Car[] = [this.subaru, this.honda, this.bmw];
+
+  constructor() {}
+
   // NEW CODE
   getCars() {
     return this.cars;
   }
- }
+}
